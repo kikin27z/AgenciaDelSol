054ae5c6ee972b8683018ec0f3e3c9989965f3dc
kikin27z <karimfv04@gmail.com>
2024-03-16 04:00:58 -0700

    Cree la ventana principal y el JFrame
    Se hizo la ventana principal del programa

---
+++ AgenciaDelSol/src/main/java/org/itson/bdavanzadas/agenciadelsol/AgenciaDelSol.java
@@ -0,0 +1,17 @@
+package org.itson.bdavanzadas.agenciadelsol;
+
+/**
+ *
+ * @author José Karim Franco Valencia - 245138
+ * @author Jesus Rene Gonzalez Castro - 247336
+ * @author Gael Rafael Castro Molina - 244802
+ */
+public class AgenciaDelSol {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        
+        ventana.cambiarVistaInicio();
+        ventana.setVisible(true);
+    }
+}
